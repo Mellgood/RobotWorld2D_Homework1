52f0927da525b96906296b05169fbfd124eeaf81
Carlo Centofanti <carlocentos@gmail.com>
2017-10-19 17:30:54 +0200

    Benchmark now runs on the same world than main run

--- Main.py
+++ Main.py
@@ -1,5 +1,7 @@
 import time
 
+import copy
+
 from model.World import World
 __height = 10
 __width = 10
@@ -11,9 +13,13 @@ __wallSymbol = '#'
 __foodSymbol = 'F' # I used 'F' instead of 'o' due to readability problems :)
 __robotSymbol ='R' #Remember to add number to conform on R# format ( even R### format is supported from map atm)
 
+__worldCopyForBenchmark = World(__height, __width)
+
 
 def main():
     world = World(__height, __width)
+    __worldCopyForBenchmark = copy.deepcopy(world)
+
 
     world.placeRandomObject(__wallSymbol,__wallNumber)
     world.placeRandomObject(__foodSymbol,__foodNumber)
@@ -25,7 +31,7 @@ def main():
     print()
     print()
 
-    foodEaten = 0
+    foodEaten = __foodNumber - world.getNumberOfFoodToFind()
     turno=1
     while foodEaten < __foodNumber and turno < __foodNumber*100: #after all these turns i stop the simulation to avoid loops
         #input('Press enter to continue: ')
@@ -45,20 +51,20 @@ def main():
 
 
     print('END OF THE GAME: ', foodEaten, ' F has been eaten and ', world.getNumberOfFoodToFind(), ' F are still on the map')
-    world.printScoreboard('SCORE')
+    #world.printScoreboard('SCORE')
     print()
 
 
 def noPrint():
-    world = World(__height, __width)
+    world = __worldCopyForBenchmark
 
     world.placeRandomObject(__wallSymbol, __wallNumber)
     world.placeRandomObject(__foodSymbol, __foodNumber)
     world.placeRandomObject(__robotSymbol, __robotNumber)
 
-    world.printMap()
+    #world.printMap()
 
-    foodEaten = 0
+    foodEaten = __foodNumber - world.getNumberOfFoodToFind()
     turno = 1
     while foodEaten < __foodNumber and turno < __foodNumber * 100:  # after all these turns i stop the simulation to avoid loops
         # input('Press enter to continue: ')
@@ -68,6 +74,7 @@ def noPrint():
 
     print('END OF THE GAME: ', foodEaten, ' F has been eaten and ', world.getNumberOfFoodToFind(),
           ' F are still on the map')
+    world.printMap()
     world.printScoreboard('SCORE')
     print()
 
@@ -80,7 +87,7 @@ if __name__ == "__main__":
     print("--- Execution time: %s seconds ---" % (exec_time1))
     inStr = None
     while not (inStr == 'Y' or inStr == 'N'):
-        inStr = input('A lot of time is wasted on printing maps every single turn. Do you want to start a simulation without printing intermediate maps? [Y/N]')
+        inStr = input('A lot of time is wasted on printing maps every single turn. Do you want to restart the simulation without printing intermediate maps? [Y/N]')
         inStr = inStr.upper()
 
     if inStr == 'Y':
@@ -88,4 +95,4 @@ if __name__ == "__main__":
         noPrint()
         exec_time2 = time.time() - start_time2
         print("--- Execution time (no print): %s seconds ---" % exec_time2)
-        print("It has taken ", exec_time1 - exec_time2, 'less seconds to perform the same task!!')
+        print("It has taken ", exec_time1 - exec_time2, 'less seconds to perform the same task!!\nNote that due to impossibility to repeat robots behaviour, scoreboard and moves can be significatly different')
